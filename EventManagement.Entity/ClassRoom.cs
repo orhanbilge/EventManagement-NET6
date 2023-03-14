0ef76cba764e9138f3e526ea319268ffbaefacac
Orhan Bilge <orhan@bilge.pro>
2023-03-14 14:14:02 +0300

    Creating Entities

---
+++ EventManagement.Entity/ClassRoom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Entity;
+
+public class ClassRoom : BaseEntity
+{
+    public string Name { get; set; }
+}
---
+++ EventManagement.Entity/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Entity;
+
+public class Teacher : BaseEntity
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+}
