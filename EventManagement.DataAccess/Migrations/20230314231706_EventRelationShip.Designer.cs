853e27e7076eced862b637f37683161d49cfb7c2
Orhan Bilge <orhan@bilge.pro>
2023-03-15 02:22:26 +0300

    Event relationship migration

--- EventManagement.DataAccess/Migrations/20230314231706_EventRelationShip.Designer.cs
+++ EventManagement.DataAccess/Migrations/20230314231706_EventRelationShip.Designer.cs
@@ -4,6 +4,7 @@ using EventManagement.DataAccess.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventManagement.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230314231706_EventRelationShip")]
+    partial class EventRelationShip
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EventManagement.DataAccess/Migrations/20230314231706_EventRelationShip.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagement.DataAccess.Migrations
+{
+    public partial class EventRelationShip : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClassRoomId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "LessonId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ClassRoomId",
+                table: "Events",
+                column: "ClassRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_LessonId",
+                table: "Events",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_TeacherId",
+                table: "Events",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_ClassRooms_ClassRoomId",
+                table: "Events",
+                column: "ClassRoomId",
+                principalTable: "ClassRooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Lessons_LessonId",
+                table: "Events",
+                column: "LessonId",
+                principalTable: "Lessons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Teachers_TeacherId",
+                table: "Events",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_ClassRooms_ClassRoomId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Lessons_LessonId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Teachers_TeacherId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ClassRoomId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_LessonId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_TeacherId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ClassRoomId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "LessonId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Events");
+        }
+    }
+}
