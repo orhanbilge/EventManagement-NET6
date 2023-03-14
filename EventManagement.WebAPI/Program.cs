0a96d8e44f707382c0959eef0935412291681b85
Orhan Bilge <orhan@bilge.pro>
2023-03-14 15:48:53 +0300

    Configuring versioning system

--- EventManagement.WebAPI/Program.cs
+++ EventManagement.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using EventManagement.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(_ =>
+{
+    _.DefaultApiVersion = new ApiVersion(1, 0);
+    _.AssumeDefaultVersionWhenUnspecified = true;
+});
 builder.Services.AddDbContext<AppDbContext>(config => config.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
